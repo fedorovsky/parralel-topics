1134bdcf59c7dd777de7d1c66740a87cde81ef8d
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-17 10:55:25 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import TopicList from './components/routes/TopicList';
 import Topic from './components/routes/Topic';
 import Home from './components/routes/Home';
+import Container from '@material-ui/core/Container';
 import { Switch, Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Header />
-    <div>
+    <Container>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/topic-list" component={TopicList} />
         <Route path="/topic" component={Topic} />
       </Switch>
-    </div>
+    </Container>
+    <Footer />
   </div>
 );
 
