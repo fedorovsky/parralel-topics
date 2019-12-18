90f36d5b0de0b6b84899a075ff3dc66deabe515e
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-18 10:07:03 +0200

    build

---
+++ build/precache-manifest.ec6ea9f9ee9da17bf44db32ff110a0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b0b956a807ab1a4aafb560cc3dd57c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c81fee49e12d319826b",
+    "url": "/static/js/2.21ce1e70.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.21ce1e70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bec552b9a17ee2d499cc",
+    "url": "/static/js/main.7125007b.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
--- src/components/routes/Topic.tsx
+++ src/components/routes/Topic.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
+import TopicCard from '../TopicCard';
 
-const Topic = () => <h1>Topic</h1>;
+const Topic = () => (
+  <div>
+    <TopicCard />
+    <TopicCard />
+  </div>
+);
 
 export default Topic;
