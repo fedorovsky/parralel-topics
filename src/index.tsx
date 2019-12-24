bc5f04e75d10c8111a563cc34d0ce8c292cf1f1a
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-24 15:42:00 +0200

    resize

--- src/index.tsx
+++ src/index.tsx
@@ -27,5 +27,11 @@ ReactDOM.render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
