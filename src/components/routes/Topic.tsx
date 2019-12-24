e635b9c9ea75dbbe1fa18810a0e4069064df465f
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-24 16:26:57 +0200

    topic

--- src/components/routes/Topic.tsx
+++ src/components/routes/Topic.tsx
@@ -1,21 +1,155 @@
 import * as React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import TopicCard from '../TopicCard';
+import {
+  // withStyles,
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core/styles';
 
-const StyledGrid = withStyles({
-  root: {
-    border: '1px solid green',
-    height: 'calc(var(--vh, 1vh) * 100 - 64px)',
-  },
-})(Grid);
+// const StyledGrid = withStyles({
+//   root: {
+//     border: '1px solid green',
+//     height: 'calc(var(--vh, 1vh) * 100 - 64px)',
+//   },
+// })(Grid);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      border: '1px solid green',
+      height: 'calc(var(--vh, 1vh) * 100 - 64px)',
+    },
+    item: {
+      border: '1px solid red',
+      height: '50%',
+      overflow: 'auto',
+    },
+  }),
+);
 
 const Topic = () => {
+  const classes = useStyles();
   return (
-    <StyledGrid container direction="column" alignItems="stretch">
-      <TopicCard />
-      <TopicCard />
-    </StyledGrid>
+    <div className={classes.root}>
+      <div className={classes.item}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
+        architecto, consectetur commodi vel omnis inventore odio maxime.
+        Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
+        perferendis aperiam sint, quis architecto voluptates impedit quasi
+        eligendi labore ea repudiandae incidunt vero doloribus cum quia?
+        Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
+        quidem quo, perferendis saepe animi vero reiciendis alias atque optio
+        nihil molestias omnis consectetur voluptates. Fugiat mollitia amet error
+        dolor, pariatur officia molestiae ipsum, voluptate veniam maxime cumque
+        nostrum quidem voluptatem culpa neque ducimus quaerat asperiores saepe
+        dolorum quos nemo! Voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Aliquam necessitatibus animi illo beatae saepe.
+        Laborum voluptates aspernatur architecto, consectetur commodi vel omnis
+        inventore odio maxime. Corrupti, aliquam explicabo esse dicta
+        accusantium ducimus non, perferendis aperiam sint, quis architecto
+        voluptates impedit quasi eligendi labore ea repudiandae incidunt vero
+        doloribus cum quia? Cupiditate nulla, sapiente incidunt pariatur sequi
+        esse veniam ducimus quidem quo, perferendis saepe animi vero reiciendis
+        alias atque optio nihil molestias omnis consectetur voluptates. Fugiat
+        mollitia amet error dolor, pariatur officia molestiae ipsum, voluptate
+        veniam maxime cumque nostrum quidem voluptatem culpa neque ducimus
+        quaerat asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Aliquam necessitatibus
+        animi illo beatae saepe. Laborum voluptates aspernatur architecto,
+        consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
+        explicabo esse dicta accusantium ducimus non, perferendis aperiam sint,
+        quis architecto voluptates impedit quasi eligendi labore ea repudiandae
+        incidunt vero doloribus cum quia? Cupiditate nulla, sapiente incidunt
+        pariatur sequi esse veniam ducimus quidem quo, perferendis saepe animi
+        vero reiciendis alias atque optio nihil molestias omnis consectetur
+        voluptates. Fugiat mollitia amet error dolor, pariatur officia molestiae
+        ipsum, voluptate veniam maxime cumque nostrum quidem voluptatem culpa
+        neque ducimus quaerat asperiores saepe dolorum quos nemo! Voluptatibus.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
+        architecto, consectetur commodi vel omnis inventore odio maxime.
+        Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
+        perferendis aperiam sint, quis architecto voluptates impedit quasi
+        eligendi labore ea repudiandae incidunt vero doloribus cum quia?
+        Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
+        quidem quo, perferendis saepe animi vero reiciendis alias atque optio
+        nihil molestias omnis consectetur voluptates. Fugiat mollitia amet error
+        dolor, pariatur officia molestiae ipsum, voluptate veniam maxime cumque
+        nostrum quidem voluptatem culpa neque ducimus quaerat asperiores saepe
+        dolorum quos nemo! Voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Aliquam necessitatibus animi illo beatae saepe.
+        Laborum voluptates aspernatur architecto, consectetur commodi vel omnis
+        inventore odio maxime. Corrupti, aliquam explicabo esse dicta
+        accusantium ducimus non, perferendis aperiam sint, quis architecto
+        voluptates impedit quasi eligendi labore ea repudiandae incidunt vero
+        doloribus cum quia? Cupiditate nulla, sapiente incidunt pariatur sequi
+        esse veniam ducimus quidem quo, perferendis saepe animi vero reiciendis
+        alias atque optio nihil molestias omnis consectetur voluptates. Fugiat
+        mollitia amet error dolor, pariatur officia molestiae ipsum, voluptate
+        veniam maxime cumque nostrum quidem voluptatem culpa neque ducimus
+        quaerat asperiores saepe dolorum quos nemo! Voluptatibus.
+      </div>
+      <div className={classes.item}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
+        architecto, consectetur commodi vel omnis inventore odio maxime.
+        Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
+        perferendis aperiam sint, quis architecto voluptates impedit quasi
+        eligendi labore ea repudiandae incidunt vero doloribus cum quia?
+        Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
+        quidem quo, perferendis saepe animi vero reiciendis alias atque optio
+        nihil molestias omnis consectetur voluptates. Fugiat mollitia amet error
+        dolor, pariatur officia molestiae ipsum, voluptate veniam maxime cumque
+        nostrum quidem voluptatem culpa neque ducimus quaerat asperiores saepe
+        dolorum quos nemo! Voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Aliquam necessitatibus animi illo beatae saepe.
+        Laborum voluptates aspernatur architecto, consectetur commodi vel omnis
+        inventore odio maxime. Corrupti, aliquam explicabo esse dicta
+        accusantium ducimus non, perferendis aperiam sint, quis architecto
+        voluptates impedit quasi eligendi labore ea repudiandae incidunt vero
+        doloribus cum quia? Cupiditate nulla, sapiente incidunt pariatur sequi
+        esse veniam ducimus quidem quo, perferendis saepe animi vero reiciendis
+        alias atque optio nihil molestias omnis consectetur voluptates. Fugiat
+        mollitia amet error dolor, pariatur officia molestiae ipsum, voluptate
+        veniam maxime cumque nostrum quidem voluptatem culpa neque ducimus
+        quaerat asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Aliquam necessitatibus
+        animi illo beatae saepe. Laborum voluptates aspernatur architecto,
+        consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
+        explicabo esse dicta accusantium ducimus non, perferendis aperiam sint,
+        quis architecto voluptates impedit quasi eligendi labore ea repudiandae
+        incidunt vero doloribus cum quia? Cupiditate nulla, sapiente incidunt
+        pariatur sequi esse veniam ducimus quidem quo, perferendis saepe animi
+        vero reiciendis alias atque optio nihil molestias omnis consectetur
+        voluptates. Fugiat mollitia amet error dolor, pariatur officia molestiae
+        ipsum, voluptate veniam maxime cumque nostrum quidem voluptatem culpa
+        neque ducimus quaerat asperiores saepe dolorum quos nemo! Voluptatibus.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
+        architecto, consectetur commodi vel omnis inventore odio maxime.
+        Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
+        perferendis aperiam sint, quis architecto voluptates impedit quasi
+        eligendi labore ea repudiandae incidunt vero doloribus cum quia?
+        Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
+        quidem quo, perferendis saepe animi vero reiciendis alias atque optio
+        nihil molestias omnis consectetur voluptates. Fugiat mollitia amet error
+        dolor, pariatur officia molestiae ipsum, voluptate veniam maxime cumque
+        nostrum quidem voluptatem culpa neque ducimus quaerat asperiores saepe
+        dolorum quos nemo! Voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Aliquam necessitatibus animi illo beatae saepe.
+        Laborum voluptates aspernatur architecto, consectetur commodi vel omnis
+        inventore odio maxime. Corrupti, aliquam explicabo esse dicta
+        accusantium ducimus non, perferendis aperiam sint, quis architecto
+        voluptates impedit quasi eligendi labore ea repudiandae incidunt vero
+        doloribus cum quia? Cupiditate nulla, sapiente incidunt pariatur sequi
+        esse veniam ducimus quidem quo, perferendis saepe animi vero reiciendis
+        alias atque optio nihil molestias omnis consectetur voluptates. Fugiat
+        mollitia amet error dolor, pariatur officia molestiae ipsum, voluptate
+        veniam maxime cumque nostrum quidem voluptatem culpa neque ducimus
+        quaerat asperiores saepe dolorum quos nemo! Voluptatibus.
+      </div>
+    </div>
   );
 };
 
