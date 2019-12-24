c79b286d5d74a0e841cab104e3f703965522736a
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-24 15:10:32 +0200

    add withStyles

--- src/components/routes/Topic.tsx
+++ src/components/routes/Topic.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 import TopicCard from '../TopicCard';
 
-const Topic = () => (
-  <div>
-    <TopicCard />
-    <TopicCard />
-  </div>
-);
+const StyledGrid = withStyles({
+  root: {
+    border: '1px solid green',
+    height: 'calc(100vh - 64px)',
+  },
+})(Grid);
+
+const Topic = () => {
+  return (
+    <StyledGrid container direction="column" alignItems="stretch">
+      <TopicCard />
+      <TopicCard />
+    </StyledGrid>
+  );
+};
 
 export default Topic;
--- src/theme.ts
+++ src/theme.ts
@@ -1,22 +1,20 @@
-import { red } from '@material-ui/core/colors';
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
 import { createMuiTheme } from '@material-ui/core/styles';
 
-// A custom theme for this app
+const breakpoints = createBreakpoints({});
+
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#556cd6'
+  overrides: {
+    // Header
+    MuiToolbar: {
+      regular: {
+        [breakpoints.up('xs')]: {
+          minHeight: 64,
+        },
+      },
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: '#fff'
-    }
-  }
+  },
+  palette: {},
 });
 
 export default theme;
