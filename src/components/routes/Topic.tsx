b423cab5e6c3b737b5fb40654456afce995481b0
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2019-12-25 20:49:51 +0200

    add Card

--- src/components/routes/Topic.tsx
+++ src/components/routes/Topic.tsx
@@ -7,6 +7,8 @@ import {
 } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { RouteComponentProps } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import useSyncScroll from 'react-use-sync-scroll';
 
 // const StyledGrid = withStyles({
@@ -21,15 +23,14 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       // 64px = Header height
       // var(--vh, 1vh) = height 1vh
-      border: '1px solid green',
+      // border: '1px solid green',
       height: 'calc(var(--vh, 1vh) * 100 - 64px)',
       display: 'flex',
       flexDirection: 'column',
+      justifyContent: 'space-between',
     },
     item: {
-      marginBottom: 10,
-      border: '1px solid red',
-      height: '50%',
+      height: '49%',
       overflow: 'auto',
     },
   }),
@@ -55,134 +56,86 @@ const Topic: React.FC<RouteComponentProps<RouterParams>> = props => {
 
   return (
     <div className={classes.root}>
-      <div className={classes.item} ref={ref1}>
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-          necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
-          architecto, consectetur commodi vel omnis inventore odio maxime.
-          Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
-          perferendis aperiam sint, quis architecto voluptates impedit quasi
-          eligendi labore ea repudiandae incidunt vero doloribus cum quia?
-          Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
-          quidem quo, perferendis saepe animi vero reiciendis alias atque optio
-          nihil molestias omnis consectetur voluptates. Fugiat mollitia amet
-          error dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus.
-        </Typography>
-      </div>
-      <div className={classes.item} ref={ref2}>
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-          necessitatibus animi illo beatae saepe. Laborum voluptates aspernatur
-          architecto, consectetur commodi vel omnis inventore odio maxime.
-          Corrupti, aliquam explicabo esse dicta accusantium ducimus non,
-          perferendis aperiam sint, quis architecto voluptates impedit quasi
-          eligendi labore ea repudiandae incidunt vero doloribus cum quia?
-          Cupiditate nulla, sapiente incidunt pariatur sequi esse veniam ducimus
-          quidem quo, perferendis saepe animi vero reiciendis alias atque optio
-          nihil molestias omnis consectetur voluptates. Fugiat mollitia amet
-          error dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus. Lorem ipsum dolor
-          sit amet, consectetur adipisicing elit. Aliquam necessitatibus animi
-          illo beatae saepe. Laborum voluptates aspernatur architecto,
-          consectetur commodi vel omnis inventore odio maxime. Corrupti, aliquam
-          explicabo esse dicta accusantium ducimus non, perferendis aperiam
-          sint, quis architecto voluptates impedit quasi eligendi labore ea
-          repudiandae incidunt vero doloribus cum quia? Cupiditate nulla,
-          sapiente incidunt pariatur sequi esse veniam ducimus quidem quo,
-          perferendis saepe animi vero reiciendis alias atque optio nihil
-          molestias omnis consectetur voluptates. Fugiat mollitia amet error
-          dolor, pariatur officia molestiae ipsum, voluptate veniam maxime
-          cumque nostrum quidem voluptatem culpa neque ducimus quaerat
-          asperiores saepe dolorum quos nemo! Voluptatibus.
-        </Typography>
-      </div>
+      <Card className={classes.item} ref={ref1} elevation={2}>
+        <CardContent>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className={classes.item} ref={ref2} elevation={2}>
+        <CardContent>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            necessitatibus animi illo beatae saepe.
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 };
