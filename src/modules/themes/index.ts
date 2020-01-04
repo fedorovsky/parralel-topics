cd8452f2b7697ff648fd893a0194ffb19353ed2c
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2020-01-04 12:34:44 +0200

    async action

--- src/modules/themes/index.ts
+++ src/modules/themes/index.ts
@@ -1,4 +1,4 @@
-import { ActionCreator, Reducer, Dispatch } from 'redux';
+import { Reducer } from 'redux';
 import { createSelector } from 'reselect';
 import { ThunkAction } from 'redux-thunk';
 import { RootState } from 'redux/reducer';
@@ -71,20 +71,20 @@ interface ThemesSuccess {
 }
 type ActionType = ThemesRequest | ThemesSuccess;
 
-type FetchPosts = ThunkAction<void, ThemeState, void, ActionType>;
-const fetchPosts = (): FetchPosts => {
-  return async (dispatch: Dispatch<ActionType>) => {
+type ThunkResult<R> = ThunkAction<R, ThemeState, void, ActionType>;
+
+const fetchPosts = (): ThunkResult<void> => {
+  return async dispatch => {
     const posts = await fetch('https://jsonplaceholder.typicode.com/posts');
     dispatch({
       type: THEMES_REQUEST,
     });
-    console.log('POSTS', posts);
+    console.log('[POSTS]', posts);
   };
 };
 
-type FetchThemeList = ThunkAction<void, ThemeState, void, ActionType>;
-export const fetchThemeList = (): FetchThemeList => {
-  return async (dispatch: Dispatch<ActionType>) => {
+export const fetchThemeList = (): ThunkResult<void> => {
+  return async dispatch => {
     dispatch(fetchPosts());
     dispatch({
       type: THEMES_REQUEST,
