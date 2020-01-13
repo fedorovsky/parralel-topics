44ff6ff1a51480dc27a3a432857b262c2b642352
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2020-01-13 21:21:37 +0200

    add ThunkMiddleware type

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, AnyAction } from 'redux';
+import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { rootReducer } from './reducer';
+import { rootReducer, RootState } from './reducer';
 import history from 'utils/history';
 
 const enhancer = composeWithDevTools(
-  applyMiddleware(thunk, routerMiddleware(history)),
+  applyMiddleware(
+    thunk as ThunkMiddleware<RootState, AnyAction>,
+    routerMiddleware(history),
+  ),
 );
 
 const store = createStore(rootReducer, enhancer);
