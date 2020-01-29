33df504825e33b15822ca45345583261587ba1a0
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2020-01-29 10:45:59 +0200

    add alert login

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { connect, ConnectedProps } from 'react-redux';
+import { Alert } from '@material-ui/lab';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { RootState } from 'redux/reducer';
-import { register } from 'modules/auth';
+import { register, getError } from 'modules/auth';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,7 +21,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const Register: React.FC<PropsFromRedux> = ({ register }) => {
+const Register: React.FC<PropsFromRedux> = ({ register, error }) => {
   const classes = useStyles();
   const [name, setName] = React.useState<string>('');
   const [email, setEmail] = React.useState<string>('');
@@ -35,49 +36,57 @@ const Register: React.FC<PropsFromRedux> = ({ register }) => {
   };
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField
-        className={classes.field}
-        fullWidth
-        value={name}
-        onChange={e => setName(e.target.value)}
-        label="Name"
-        variant="outlined"
-        type="text"
-      />
-      <TextField
-        className={classes.field}
-        fullWidth
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        label="Email"
-        variant="outlined"
-        type="text"
-      />
-      <TextField
-        className={classes.field}
-        fullWidth
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        label="Password"
-        variant="outlined"
-        type="password"
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        fullWidth
-        onClick={handleRegister}
-      >
-        Register
-      </Button>
-    </form>
+    <div>
+      {error && <Alert severity="error">{error}</Alert>}
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          className={classes.field}
+          fullWidth
+          value={name}
+          onChange={e => setName(e.target.value)}
+          label="Name"
+          variant="outlined"
+          type="text"
+        />
+        <TextField
+          className={classes.field}
+          fullWidth
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          label="Email"
+          variant="outlined"
+          type="text"
+        />
+        <TextField
+          className={classes.field}
+          fullWidth
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          label="Password"
+          variant="outlined"
+          type="password"
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+          onClick={handleRegister}
+        >
+          Register
+        </Button>
+      </form>
+    </div>
   );
 };
 
-const connector = connect((state: RootState) => ({}), {
-  register,
-});
+const connector = connect(
+  (state: RootState) => (state: RootState) => ({
+    error: getError(state),
+  }),
+  {
+    register,
+  },
+);
 
 export default connector(Register);
